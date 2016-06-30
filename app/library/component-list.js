4c38321119280c7d7f48de27f64d4fe8de962824
Faisal <falquaddoomi@ucla.edu>
2016-06-30 08:22:05 -0400

    Temporarily removed 'rhythm map', as it's beyond broken currently

--- app/library/component-list.js
+++ app/library/component-list.js
@@ -6,6 +6,6 @@ export default [
   {name: 'Top Ten Alters', type: 'vizmods/top-alters', icon: 'handler glyphicon glyphicon-stats', invisible: false},
   {name: 'Top Alters (Pie)', type: 'vizmods/top-alters-pie', icon: 'handler fa fa-pie-chart', invisible: false},
   {name: 'Word Cloud', type: 'vizmods/word-cloud', icon: 'handler glyphicon glyphicon-cloud', invisible: false},
-  {name: 'Heatmap Calendar', type: 'vizmods/heat-map', icon: 'handler glyphicon glyphicon-th', invisible: false},
-  {name: 'Rhythm Map', type: 'vizmods/rhythm-map', icon: 'handler glyphicon glyphicon-time', invisible: false}
+  {name: 'Heatmap Calendar', type: 'vizmods/heat-map', icon: 'handler glyphicon glyphicon-th', invisible: false}
+  // {name: 'Rhythm Map', type: 'vizmods/rhythm-map', icon: 'handler glyphicon glyphicon-time', invisible: false}
 ];
