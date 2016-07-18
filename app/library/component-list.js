2a55d4d73d1b60bf28fd4fd001458cf59f42f45a
Faisal <falquaddoomi@ucla.edu>
2016-07-18 10:50:58 -0400

    Temporarily disabled heatmap and rhythm map on account of them being broken

--- app/library/component-list.js
+++ app/library/component-list.js
@@ -6,7 +6,7 @@ export default [
   {name: 'Top Ten Alters', type: 'vizmods/top-alters', icon: 'handler glyphicon glyphicon-stats', invisible: false},
   {name: 'Top Alters (Pie)', type: 'vizmods/top-alters-pie', icon: 'handler fa fa-pie-chart', invisible: false},
   {name: 'Word Cloud', type: 'vizmods/word-cloud', icon: 'handler glyphicon glyphicon-cloud', invisible: false},
-  {name: 'Heatmap Calendar', type: 'vizmods/heat-map', icon: 'handler glyphicon glyphicon-th', invisible: false},
-  {name: 'Topic Timeline', type: 'vizmods/topic-line', icon: 'handler glyphicon glyphicon-text-size', invisible: false}
-  // {name: 'Rhythm Map', type: 'vizmods/rhythm-map', icon: 'handler glyphicon glyphicon-time', invisible: true}
+  {name: 'Heatmap Calendar', type: 'vizmods/heat-map', icon: 'handler glyphicon glyphicon-th', invisible: true},
+  {name: 'Topic Timeline', type: 'vizmods/topic-line', icon: 'handler glyphicon glyphicon-text-size', invisible: false},
+  {name: 'Rhythm Map', type: 'vizmods/rhythm-map', icon: 'handler glyphicon glyphicon-time', invisible: true}
 ];
