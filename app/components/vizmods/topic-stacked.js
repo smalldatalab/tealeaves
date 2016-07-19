0cafbc5d7935c256ebfe9a1cfbecb03715e384ab
Faisal <falquaddoomi@ucla.edu>
2016-07-19 17:30:56 -0400

    Removed commented-out cruft from topic-stacked

--- app/components/vizmods/topic-stacked.js
+++ app/components/vizmods/topic-stacked.js
@@ -236,35 +236,5 @@ export default BaseMod.extend({
             _this.$(".loader").hide();
           });
       });
-
-    // attempt to hit the eaf API
-    // this returns a promise, which we'll use when it resolves
-    /*
-     this.get('eaf_api').query('mail_message', { min_date: tools.apiTZDateTime(start_date), max_date: tools.apiTZDateTime(end_date) })
-     .then(function(data) {
-     // we need to collapse on the user's name after removing single-level parentheticals
-     var revised_counts = tools.countBy(data.objects.map((x) => x.from_field),
-     function(k) { return k.replace(emails, '').replace(stripquotes, '').replace(stripparens, '').trim(); });
-
-     // convert the key:value map into a [{label: <key>, value: <value>},...] array, sigh
-     revised_counts = Object.keys(revised_counts).map(function (key, idx) {
-     return { id: idx+1, name: key, count: revised_counts[key] };
-     });
-
-     var total_mails = d3.max(revised_counts, function(x) { return x.count; });
-     revised_counts.sort(function(a,b) { return b.count-a.count; });
-
-     revised_counts = revised_counts.map((item,idx) => { item['idx'] = idx+1; return item; });
-
-     // update list that'll be reflected in this component's template immediately
-     _this.set('total_mails', total_mails);
-     _this.set('alters', revised_counts.slice(0,10));
-
-     // _this.$(".d3box").html(data.map(function(x) { return x.address.replace(emails, "").trim() + " (" + x.count + ")"; }).join("<br />"));
-     })
-     .finally(function() {
-     _this.$(".loader").hide();
-     });
-     */
   }
 });
