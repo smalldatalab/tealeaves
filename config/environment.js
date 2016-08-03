b6cd8dea377dd897333fe4edf2198cc692d0a432
Faisal <falquaddoomi@ucla.edu>
2016-08-03 16:57:23 -0400

    Added some dependencies for local settings storage iirc

--- config/environment.js
+++ config/environment.js
@@ -30,6 +30,12 @@ module.exports = function(environment) {
       'img-src': "'self'",
       'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com
       'media-src': "'self'"
+    },
+
+    localSettings: {
+      serializer: 'json',
+      adapter: 'local-storage',
+      prefix: 'tealeaves/'
     }
   };
 
