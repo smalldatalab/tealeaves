4ffb224e5953439eebe56caa42df72cec5ee26c4
Faisal <falquaddoomi@ucla.edu>
2016-08-03 16:57:39 -0400

    Removes remote directory before scp'ing new build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ember build --environment=production
+ssh -i ~/.ssh/eaf-related.pem ec2-user@tealeaves.smalldata.io <<< "rm -rf /var/www/tealeaves/dist"
 scp -r -i ~/.ssh/eaf-related.pem dist ec2-user@tealeaves.smalldata.io:/var/www/tealeaves
